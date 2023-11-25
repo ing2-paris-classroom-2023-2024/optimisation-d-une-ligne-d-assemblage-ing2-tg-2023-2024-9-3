62f476b1b280be27d6610bb580757ca2ef284228
1l8c3 <alice.mycoudert@gmail.com>
2023-11-25 15:12:29 +0100

    lecture precedences pas fonctionnel

--- alice.c
+++ alice.c
@@ -1,35 +1,69 @@
 #include <stdio.h>
+#define MAX_OPERATIONS 31
 
 int ouvrir_fichiers() {
-    FILE *file1, *file2, *file3;
+    FILE *precedences, *operations, *tempscycle;
 
-    file1 = fopen("../precedence.txt", "r");
-    file2 = fopen("../operations.txt", "r");
-    file3 = fopen("../exclusions.txt", "r");
+    precedences = fopen("../precedence.txt", "r");
+    operations = fopen("../operations.txt", "r");
+    tempscycle = fopen("../temps_cycle.txt", "r");
 
 
-    if (file1 == NULL || file2 == NULL || file3 == NULL) {
+    if (precedences == NULL || operations == NULL || tempscycle == NULL) {
 
-        if (file1 != NULL) fclose(file1);
-        if (file2 != NULL) fclose(file2);
-        if (file3 != NULL) fclose(file3);
+        if (precedences != NULL) fclose(precedences);
+        if (operations != NULL) fclose(operations);
+        if (tempscycle != NULL) fclose(tempscycle);
 
         return 1;
     }
-    fclose(file1);
-    fclose(file2);
-    fclose(file3);
 
     return 0;
 }
 
+typedef struct {
+    char nom_operation[50];
+    int temps_execution;
+} Operation;
+
+void lire_operations(FILE *fichier, Operation operations[], int *nombre_operations) {
+    while (fscanf(fichier, "%s %d", operations[*nombre_operations].nom_operation, &operations[*nombre_operations].temps_execution) == 2) {
+        (*nombre_operations)++;
+    }
+}
+
+void lire_contraintes(FILE *fichier, char contraintes[][50], int *nombre_contraintes) {
+    while (fscanf(fichier, "%s %s", contraintes[*nombre_contraintes], contraintes[*nombre_contraintes + 1]) == 2) {
+        (*nombre_contraintes) += 2;
+    }
+}
+
+void lire_temps_cycle(FILE *fichier, int *t_cycle) {
+    fscanf(fichier, "%*s %d", t_cycle);
+}
+
+
 int main() {
+
     int resultat = ouvrir_fichiers();
     if (resultat == 0) {
         printf("Pas de pb.\n");
     } else {
         printf("Erreur\n");
     }
+    Operation operations[MAX_OPERATIONS];
+    char contraintes[MAX_OPERATIONS][50];
+    int nombre_operations = 0;
+    int nombre_contraintes = 0;
+    int t_cycle;
+
+    lire_operations(operations, operations, &nombre_operations);
+    //lire_contraintes(precedences, contraintes, &nombre_contraintes);
+    //lire_temps_cycle(fichier_temps_cycle, &t_cycle);
+
+   // fclose(precedences);
+    fclose(operations);
+   // fclose(tempscycle);
 
     return 0;
 }
