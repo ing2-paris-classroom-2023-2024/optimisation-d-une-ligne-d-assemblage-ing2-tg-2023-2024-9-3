2291c21b0dacbd55368a6a7563bfa1eab533f01b
edouardmnt <edouardmenut04@gmail.com>
2023-12-01 11:59:30 +0100

    structure et code repartion exclusion

--- edouard.h
+++ edouard.h
@@ -4,23 +4,28 @@
 
 #ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_9_33_EDOUARD_H
 #define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_9_33_EDOUARD_H
-struct t_nomSommet {
+struct t_sommet {
     char nom[50];
+    float temp_execution;
 };
-typedef struct t_nomSommet nom_sommet;
+typedef struct t_sommet sommet;
 
 struct t_exclusion {
     char sommet1[50];
     char sommet2[50];
 };
-typedef struct t_exclusion Exclusion;
+typedef struct t_exclusion exclusion;
 
 struct t_graphe {
     int numSommets;
     int **matriceAdjacence;
-    nom_sommet *sommets;
+    float **matricePonderation;
+    sommet *sommets;
+    float *temps;
     int *station;
 };
 typedef struct t_graphe Graphe;
+
+
 
 #endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_9_33_EDOUARD_H
