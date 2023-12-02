7a6c692f0621d8f98cd45ce79aa4b12227fef61c
cloclo78 <chloe.lestic@edu.ece.fr>
2023-12-02 16:20:10 +0100

    02/12 : multi contraintes premiere approche

--- multi.h
+++ multi.h
@@ -6,7 +6,8 @@
 #define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_9_3_MULTI_H
 #include "structures.h"
 #include "exclusions.h"
-#include "prectmpscycle.h"
+#include "precedence_temps_cycle.h"
+
 
 int *nouvelleRepartition(Graphe *graphe, float tempsCycle) {
     int *repartition1 = triDansStations(graphe, tempsCycle);  // Récupère la première répartition
@@ -22,10 +23,8 @@ int *nouvelleRepartition(Graphe *graphe, float tempsCycle) {
     // Vérifie et ajuste la nouvelle répartition en fonction de la deuxième répartition
     for (int i = 0; i < graphe->nombreSommets; ++i) {
         for (int j = i + 1; j < graphe->nombreSommets; ++j) {
-            // Si les sommets i et j sont dans la même station dans la repartition1
-            // mais pas dans la repartition2, scinder en 2 stations
+
             if (repartition1[i] == repartition1[j] && repartition2[i] != repartition2[j]) {
-                // Crée une nouvelle station pour le sommet j
                 for (int k = 0; k < graphe->nombreSommets; ++k) {
                     if (nouvelleRepartition[k] == repartition1[j]) {
                         nouvelleRepartition[k] = repartition1[j] + 1;
@@ -51,11 +50,11 @@ int *nouvelleRepartition(Graphe *graphe, float tempsCycle) {
 
     return nouvelleRepartition;
 }
-void affichagefinal(char* fichierTempsCycle){
+void affichagefinal(char* fichierTempsCycle) {
     printf("affichage multicontraintes\n");
     Graphe graphe;
-    float tempsCycle= liretempscycle(fichierTempsCycle);
-    nouvelleRepartition(&graphe,tempsCycle);
+    float tempsCycle = liretempscycle(fichierTempsCycle);
+    nouvelleRepartition(&graphe, tempsCycle);
 
 }
 
